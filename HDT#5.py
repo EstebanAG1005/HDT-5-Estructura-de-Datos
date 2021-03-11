e0690cabfc4e4316e6a92f56637d3553098e205b
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-11 17:58:59 -0600

    .

--- HDT#5.py
+++ HDT#5.py
@@ -1,15 +1,50 @@
+
 import simpy
 import random
-#Codigo Creado por Esteban Aldana Guerra 20591 y Kenneth Galvez 20
+#Codigo Creado por Esteban Aldana Guerra 20591 y Kenneth Galvez 20079
+
+#Codigo Basado en ejemplo dado en (Clase de una gasolinera)
 
 #Corre varias simulaciones para ver en cuento tiempo se realiza ese proceso dentro de la memoria
-def simulacion (nombre, env, RAM, Tiempo, memoria, instruccion, Cprocesos, Procesadora):
+def simulacion (nombre, env, RAM, Procesadora,instruccion, Tprocesos, Tiempo, memoria,):
     # Inicia la simulacion
     sim = True
     # Mientras que el programa este funcionando va a realizar la simulacion
-    while sim = True:
+    while sim == True:
         #Simular que obtiene el espacio en memoria
-        yield Ram.get(memoria)
+        yield RAM.get(memoria)
         #Mientras que las instrucciones sean mayores a 0
-        while instruccion > 0
+        while instruccion > 0:
+             # Se dirige al lugar donde se va a realizar el proceso
+             # pero si hay otro proceso, debe hacer cola
+            with Procesadora.request() as cola:
+                #Ya comenzo el proceso
+                yield cola
+                #Realza el proceso en un tiempo determinado
+                yield env.timeout(1)
+              
+                instruccion = instruccion - Tprocesos
+
+                Cprocesos = random.randint(1,2)
+
+                if (Cprocesos == 1):
+                  print("Se envia proceso %s a cola por %s segundos. Tiempo Actual: %s , Espacio en memoria: %s" % (nombre, Tiempo, env.now, memoria))
+                  yield env.timeout(Tiempo)
+                else:
+                  print("Se envia el proceso %s a la memoria para ver su tiempo, Tiempo %s, Tiempo Actual: %s , Espacio en memoria: %s" % (nombre, Tprocesos, env.now, memoria))
+                  RAM.put(memoria)
+                  sim = False
+                  
+env = simpy.Environment()
+RAM = simpy.Container(env, capacity=100)
+RAM.put(100)
+Procesadora = simpy.Resource(env, capacity=2)
+
+
+for i in range(6):
+    env.process(simulacion(env, i+1,RAM,Procesadora,random.randint(30,60),random.randint(8,12),3,3))
     
+
+#Se corre la simulacion
+env.run()
+
