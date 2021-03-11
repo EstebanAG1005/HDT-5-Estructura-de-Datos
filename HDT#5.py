c8b70d361e6e4be4646a56183d1e20c25a5a2615
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-11 07:56:40 -0600

    Primer Commit

---
+++ HDT#5.py
@@ -0,0 +1,15 @@
+import simpy
+import random
+#Codigo Creado por Esteban Aldana Guerra 20591 y Kenneth Galvez 20
+
+#Corre varias simulaciones para ver en cuento tiempo se realiza ese proceso dentro de la memoria
+def simulacion (nombre, env, RAM, Tiempo, memoria, instruccion, Cprocesos, Procesadora):
+    # Inicia la simulacion
+    sim = True
+    # Mientras que el programa este funcionando va a realizar la simulacion
+    while sim = True:
+        #Simular que obtiene el espacio en memoria
+        yield Ram.get(memoria)
+        #Mientras que las instrucciones sean mayores a 0
+        while instruccion > 0
+    
